bcc7a28895d92d9e342c360f49fc10fc48f8fd3a
tina073 <164857904+tina073@users.noreply.github.com>
2024-07-26 14:38:14 +0900

    Add files via upload

---
+++ js/subPage_F&Q.js
@@ -0,0 +1,20 @@
+$(function(){
+    //카테고리 버튼 체인지 컬러
+    $(".categoryBtn_wrap li").click(function(){
+        $(".categoryBtn_wrap li").removeClass();
+        $(this).addClass("on");
+    })
+
+    //게시판 목록 체인지
+    $(".categoryBtn_wrap li").click(function(){
+        const btnidx = $(this).index();
+        $(".board").eq(btnidx).show().siblings().hide();
+    })
+
+    //아코디언 박스 슬라이드
+    $(".boardWrap li").click(function(){
+        $(".accordionBox").removeClass("slide");
+        $(this).find(".accordionBox").addClass("slide");
+    })
+
+})
