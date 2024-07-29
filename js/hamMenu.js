6a4a0924315ebff32bd60b3d0d6ec0dada001f45
tina073 <164857904+tina073@users.noreply.github.com>
2024-07-29 15:41:06 +0900

    Add files via upload

--- js/hamMenu.js
+++ js/hamMenu.js
@@ -1,13 +1,23 @@
 $(function(){
-    $('.hamBtn').click(function() {
-		$(this).toggleClass('act');
-			if($(this).hasClass('act')) {
-				$('.hamBg').addClass('act');
-				$('.menu').addClass('act');
-			}
-			else {
-				$('.hamBg').removeClass('act');
-				$('.menu').removeClass('act');
-			}
+
+	$(".hamBtn").click(function(){
+		let hamBg = $(".hamBg");
+		
+		if (hamBg.css("right") === "0px") {
+			hamBg.animate({right: "-100%"});
+			$("#menu").stop().hide();
+			$("h1 img").attr("src","images/logo_b.png")
+			setTimeout(function(){
+				$(".hamBtn img").attr("src","images/icon_ham_b.svg");
+			}, 300);
+		} else {
+			hamBg.animate({right: "0"});
+			$(".hamBtn img").attr("src","images/icon_ham.svg");
+			setTimeout(function(){
+				$("#menu").stop().show().addClass("open");
+				$("h1 img").attr("src","images/logo_w.png");
+			}, 300);
+		}
 	});
+	
 })
