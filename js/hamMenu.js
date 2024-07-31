f7d287715716760d21f8a96d5b5d36e452159acd
tina073 <164857904+tina073@users.noreply.github.com>
2024-07-31 10:25:14 +0900

    Add files via upload

--- js/hamMenu.js
+++ js/hamMenu.js
@@ -1,23 +1,27 @@
 $(function(){
 
 	$(".hamBtn").click(function(){
+		//오픈 메뉴
 		let hamBg = $(".hamBg");
 		
 		if (hamBg.css("right") === "0px") {
 			hamBg.animate({right: "-100%"});
-			$("#menu").stop().hide();
-			$("h1 img").attr("src","images/logo_b.png")
-			setTimeout(function(){
-				$(".hamBtn img").attr("src","images/icon_ham_b.svg");
-			}, 300);
+			$(".gnb").stop().hide();
+			$("h1 img").attr("src","images/logo_b.png");
+			$(".hamBtn span").css("background-color","#000");
 		} else {
 			hamBg.animate({right: "0"});
-			$(".hamBtn img").attr("src","images/icon_ham.svg");
+			$(".hamBtn span").css("background-color","#fff");
 			setTimeout(function(){
-				$("#menu").stop().show().addClass("open");
+				$(".gnb").stop().show().addClass("open");
 				$("h1 img").attr("src","images/logo_w.png");
 			}, 300);
 		}
 	});
-	
+
+	//햄버거 버튼 x로 바뀌는 효과
+	const menuTrigger = document.querySelector('.hamBtn');
+	menuTrigger.addEventListener('click', (event) => {
+	event.currentTarget.classList.toggle('active-1');
+	});
 })
